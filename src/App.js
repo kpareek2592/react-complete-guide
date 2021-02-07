2f65bfc625a611a9bc6c4ab73d2774435a59c53c
kpareek2592 <kpareek2592@gmail.com>
2021-02-07 20:42:40 +0530

    Handling dynamic content the JS way

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
 
   togglePersonHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
+    this.setState({ showPersons: !doesShow });
   }
 
   render() {
@@ -51,23 +51,28 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Edward')}
+            changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+          {/* Use the .bind syntax more */}
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        </div>
+      )
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working.</p>
         <button style={style} onClick={this.togglePersonHandler}>Switch Name</button>
-        {
-          this.state.showPersons ?
-            <div>
-              <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-              <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, 'Edward')}
-                changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-              {/* Use the .bind syntax more */}
-              <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-            </div>
-            : null
-        }
+
+        {persons}
       </div>
     );
   };
