cf89e79f5036f489570615382a70cc48156c665a
kpareek2592 <kpareek2592@gmail.com>
2021-02-07 14:33:53 +0530

    Removing git hooks and usinf setState for state change

--- src/App.js
+++ src/App.js
@@ -1,58 +1,48 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
 
-const app = props => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Manual', age: 29 },
       { name: 'Steph', age: 26 }
     ],
     otherstate: 'some  other state value'
-  });
+  };
 
-  console.log(personsState);
-
-  const switchNameHandler = () => {
+ switchNameHandler = () => {
     // console.log('Was Clicked');
     // DON'T DO THIS: this.state.persons[0].name = 'Silver';
-    setPersonsState({
+    this.setState({
       persons: [
         { name: 'Silver', age: 28 },
         { name: 'Manual', age: 29 },
         { name: 'Roger', age: 30 }
       ],
-      otherstate: personsState.otherstate
+      otherstate: 'some other value'
     });
   };
 
+  render () {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working.</p>
-        <button onClick={switchNameHandler}>Switch Name</button>
-        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
+  };
+    
 
     // return React.createElement('div', {className: 'App'}, null, React.createElement('h1', null, 'Does this work now'));
   }
 
 
 
-  export default app;
-
-  // state = {
-  //   persons: [
-  //     { name: 'Max', age: 28 },
-  //     { name: 'Manual', age: 29 },
-  //     { name: 'Steph', age: 26 }
-  //   ],
-  //   otherstate: 'some  other state value'
-  // };
-
-  // 
+  export default App;
