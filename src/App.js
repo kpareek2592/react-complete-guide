675092751457fa0647a53b59d91382bf1093a6c0
kpareek2592 <kpareek2592@gmail.com>
2021-02-08 13:58:11 +0530

    Introducing Styled Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
   state = {
@@ -90,7 +89,7 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
+      
         <div className="App">
           <h1>Hi, I'm a React App</h1>
           <p className={classes.join(' ')}>This is really working!</p>
@@ -99,11 +98,11 @@ class App extends Component {
             onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
         </div>
-      </StyleRoot>
+      
     );
 
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default Radium(App);
+export default App;
