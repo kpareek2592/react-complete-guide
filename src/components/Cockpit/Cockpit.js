3ce39c1d98fa8559c9df6cbf679baacbd69dc1a5
kpareek2592 <kpareek2592@gmail.com>
2021-02-12 11:52:51 +0530

    useEffect() hook usage

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+useEffect(() => {
+    console.log('[Cockpit.js] use effect');
+    setTimeout(() => {
+        //alert('Saved some data to cloud');
+        console.log('Saved some data to cloud');
+    }, 1000);
+}, [props.persons])
+
     const assignedClasses = [];
     let btnClass = '';
     if (props.showPersons) {
