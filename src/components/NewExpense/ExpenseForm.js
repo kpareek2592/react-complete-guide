3e5536285aae873e7602d6785136410e39de9881
kpareek2592 <kpareek2592@gmail.com>
2021-07-31 17:10:32 +0530

    Code refactoring and linting

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,24 +2,24 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
-//   const [enteredTitle, setEnteredTitle] = useState('');
-//   const [enteredAmount, setEnteredAmount] = useState('');
-//   const [enteredDate, setEnteredDate] = useState('');
+  //   const [enteredTitle, setEnteredTitle] = useState('');
+  //   const [enteredAmount, setEnteredAmount] = useState('');
+  //   const [enteredDate, setEnteredDate] = useState('');
 
   const [userInput, setUserInput] = useState({
-    enteredTitle: '', 
-    enteredAmount: '', 
-    enteredDate: '' 
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
   });
 
   const titleChangeHandler = (event) => {
     setUserInput({
-        ...userInput,                       // This spread operator is used so that other state variables are not lost as we are updating only one
-        enteredTitle: event.target.value,
-    })
+      ...userInput, // This spread operator is used so that other state variables are not lost as we are updating only one
+      enteredTitle: event.target.value,
+    });
   };
 
-  const amountChangeHandler = event => {
+  const amountChangeHandler = (event) => {
     // setUserInput({
     //     ...userInput,
     //     enteredAmount: event.target.value
@@ -28,30 +28,30 @@ const ExpenseForm = (props) => {
     //  Prefer this over the above commented way to update the state as if we have many state updates then as react is scheduling these updates
     //  for later execution we may depend on an outdated value of the state. This makes us operate on latest states of the data
     setUserInput((prevState) => {
-        return { ...prevState, enteredAmount: event.target.value };
-    })
+      return { ...prevState, enteredAmount: event.target.value };
+    });
   };
 
   const dateChangeHandler = (event) => {
-    setUserInput( (prevState) => {
-        return { ...prevState, enteredDate: event.target.value };
-    })
+    setUserInput((prevState) => {
+      return { ...prevState, enteredDate: event.target.value };
+    });
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
 
     const expenseData = {
-        title: userInput.enteredTitle,
-        amount: userInput.enteredAmount,
-        date: new Date(userInput.enteredDate)
+      title: userInput.enteredTitle,
+      amount: userInput.enteredAmount,
+      date: new Date(userInput.enteredDate),
     };
 
     props.onSaveExpenseData(expenseData);
     setUserInput({
-        enteredTitle: '',
-        enteredAmount: '',
-        enteredDate: ''
+      enteredTitle: "",
+      enteredAmount: "",
+      enteredDate: "",
     });
   };
 
@@ -60,15 +60,31 @@ const ExpenseForm = (props) => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" value={userInput.enteredTitle} onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={userInput.enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" value={userInput.enteredAmount} onChange={amountChangeHandler} />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={userInput.enteredAmount}
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" value={userInput.enteredDate} onChange={dateChangeHandler} />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={userInput.enteredDate}
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,19 @@
 import React from "react";
 import ExpenseForm from "./ExpenseForm";
-import './NewExpense.css';
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
   const saveExpenseDataHandler = (enteredExpenseData) => {
-      const expenseData = {
-          ...enteredExpenseData,
-          id: Math.random().toString()
-      };
-      props.onAddExpense(expenseData);
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
   };
 
-  return(
+  return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
