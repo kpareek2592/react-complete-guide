1daa42d1661fc46389ca2b8fe1deab784266a275
kpareek2592 <kpareek2592@gmail.com>
2021-07-31 18:38:56 +0530

    Rendering Lists of Data

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const App = () => {
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses expenses={expenses} />
+      <Expenses items={expenses} />
     </div>
   );
 };
